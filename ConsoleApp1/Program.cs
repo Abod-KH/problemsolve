826b00c977fa0414670f8494b92b4ba496aca1e3
Your Name <your.email@example.com>
2025-11-26 12:52:45 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static int Wightcalc(int a, int b)
+    {
+        int years = 0;
+        int limak = a;
+        int bob = b;
+        while (limak <= bob)
+        {
+            limak *= 3;
+            bob *= 2;
+            years++;
+        }
+        return years;
+    }
+
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine(Wightcalc(4, 7));
+    }
+}
